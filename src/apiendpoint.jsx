353a3e0534f1a39174e804b8932f636d7991e9a1
Aman Sharma <droj.co@gmail.com>
2024-12-16 10:11:10 +0530

    Update apiendpoint.jsx

--- src/apiendpoint.jsx
+++ src/apiendpoint.jsx
@@ -1,6 +1,6 @@
 const apilocal="http://127.0.0.1:8000"
-const apidlobalDev="https://api.sentient.in"
-const apidlobalQA="https://apis.sentient.in"
+const apidlobalDev="https://api.sentientco.in"
+const apidlobalQA="https://apis.sentientco.in"
 
 // const currentAPI=apilocal
 const currentAPI=apidlobalDev
